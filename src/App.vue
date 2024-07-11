378bfece7e2d738fb684511239fd62fc0960784e
Kwasu <lukaszkwas96@gmail.com>
2024-07-11 14:29:25 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,8 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<template></template>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<script></script>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
+<style lang="scss" scoped>
 header {
   line-height: 1.5;
 }
