7efba27a2c21a43f229e68abb0801b58860c4271
Kwasu <lukaszkwas96@gmail.com>
2024-07-18 12:33:27 +0200

    .

--- src/components/WelcomeScreen.vue
+++ src/components/WelcomeScreen.vue
@@ -37,10 +37,38 @@ export default {
   width: 80%;
 }
 
+.buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10rem 0 4rem 0;
+}
+
 .logo {
   img {
     height: 100%;
     width: 100%;
+  }
+}
+
+button {
+  font-size: 1.4rem;
+  background: #ffbf00;
+  color: #808836;
+  border: 0.3rem solid #808836;
+  padding: 0.9rem 1.6rem;
+  display: flex;
+  align-items: center;
+  margin: 0 2rem;
+  cursor: pointer;
+  border-radius: 3rem;
+  font-weight: bold;
+  transition: all 0.3s ease;
+  &:hover {
+    transform: scale(1.1);
+  }
+  &:active {
+    transform: scale(0.95);
   }
 }
 
